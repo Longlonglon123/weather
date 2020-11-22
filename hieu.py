f3d412fb6caaaca1fd4901784001df0100e347d8
unknown <longtran>
2020-11-22 10:46:47 +0700

    push code

---
+++ hieu.py
@@ -0,0 +1,12 @@
+
+you = "hello"
+
+if you == "":
+	robot_brain = "I can't here you"
+elif you == "hello":
+	robot_brain = "hello I can help you"
+elif you == "today":
+	robot_brain = "thu 5"
+else 
+	robot_brain = "sorry I can't help you"
+print (robot_brain)
---
+++ tro_ly.py
@@ -0,0 +1,51 @@
+import speech_recognition
+
+while True:
+	robot_ear = speech_recognition.Recognizer()
+	with speech_recognition.Microphone() as mic:
+		print("Robot: I'm listening")
+		audio = robot_ear.listen(mic)
+	print("Robot:....")
+
+	try:
+		you = robot_ear.recognize_google(audio)
+	except:
+		you = ""
+
+	print ("you: " + you)
+
+
+
+	if you == " ":
+		robot_brain = "I can't here you"
+	elif "hello" in you:
+		robot_brain = "hello, I can help you"
+	elif "today" in you:
+		from datetime import date
+
+		today = date.today()
+		robot_brain = today.strftime("%B %d, %Y")
+	elif "time" in you:
+		from datetime import datetime
+
+		now = datetime.now()
+		robot_brain = now.strftime("%H:%M:%S")
+	elif "president" in you:
+		robot_brain = "Donald Trump"
+	elif  "thank you" in you:
+		robot_brain = "ohh nothing"
+	elif "bye" in you:
+		robot_brain = "goodbye"
+		robot_mouth = pyttsx3.init()
+		robot_mouth.say(robot_brain)
+		robot_mouth.runAndWait()
+		break
+	else: 
+		robot_brain = "sorry I can't help you"
+	print (robot_brain)
+
+	import pyttsx3
+
+	robot_mouth = pyttsx3.init()
+	robot_mouth.say(robot_brain)
+	robot_mouth.runAndWait()
